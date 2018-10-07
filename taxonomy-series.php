7828ddf3022577c50dd0202000df144ed788a50c
Joseph Benson <joseph.benson@alumni.usc.edu>
2018-10-07 14:00:30 -0700

    Allows filtering Series but disables infinite scrolling until it can be fixed. Default to show 100 Series Messages per Series archive page.

--- taxonomy-series.php
+++ taxonomy-series.php
@@ -7,60 +7,45 @@
  * @package ROCKHARBOR_Church
  */
 
-$campus = $_COOKIE['campus'];
-// If Campus Cookie Not set
-if (!$campus) {
-	// check for campus url parameter, ex: ?c=cotsa-mesa
-	if(isset($_GET['c'])) {
-    	$campus = $_GET['c'];
-	} else {
-		$campus_slug = get_post(269)->post_name; // Costa Mesa by default
-		$campus = $campus_slug;	
-	}
-}
-
+$campus = $_COOKIE['campus'] ?? $_GET['c'] ?? 'all';
 get_header();
 ?>
-	
+
 	<section class="series-archive">
-		<div class="container">
-			<?php the_archive_title( '<h1 class="heading">', '</h1>' ); ?>
-			
-			<!--
-			<div class="campus-filter" style="position: relative; top: auto; right: auto; margin: auto auto 30px auto;">
-				<select id="campus-filter">
-					<?php
-					$taxonomy = 'campuses';
-					$locationsargs = array (
-						'hide_empty'	=> 0,
-						'parent'        => 0,
-						'taxonomy'      => $taxonomy,
-					);
-					$locations = get_terms($locationsargs);
-						foreach($locations as $location) {?>
-						<option value="<?php echo $location->slug;?>"><?php echo $location->name;?></option>
+		<?php the_archive_title( '<h1 class="heading">', '</h1>' ); ?>
+		<div class="campus-filter" style="position: relative; margin-bottom: 50px; margin-left: auto; margin-right: auto;" >
+			<select id="campus-filter">
+				<?php
+					$locations = get_terms(array(
+						'hide_empty' => 0,
+						'parent' => 0,
+						'taxonomy' => 'campuses'
+					));
+					$locations[] = (object) ['name' => 'All Campuses', 'slug' => 'all'];
+					foreach ($locations as $location) {?>
+					<option value="<?php echo $location->slug; ?>"><?php echo $location->name; ?></option>
 					<?php } ?>
-				</select>
-			</div>
-			-->
-			
+			</select>
+		</div>
+		<?php foreach ($locations as $location): ?>
+		<div class="container campus-<?php echo $location->slug; ?>">
+
 			<?php $terms = get_queried_object()->slug;
 			$paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+			$taxQuery = ($location->slug == 'all') ? array() : array(array(
+				'taxonomy' => 'campuses',
+				'field' => 'slug',
+				'terms' => $location->slug
+			));
 			$args = array (
 				'taxonomy'			=> 'series',
 				'term'				=> $terms,
 				'post_type'         => array( 'message' ),
-				'posts_per_page'    => 10,
+				'posts_per_page'    => 100,
 				'paged' 			=> $paged,
 				'order'             => 'DESC',
 				'orderby'           => 'date',
-				/*'tax_query' => array(
-			        array(
-			            'taxonomy' => 'campuses',
-						'field' => 'slug',
-						'terms' => $campus,
-			        ),
-			    ),		*/	
+				'tax_query' => $taxQuery
 			);
 			// The Query
 			$query = new WP_Query( $args );
@@ -72,7 +57,7 @@ get_header();
 					$tags = get_the_term_list( $post->ID, 'tags', '<span>', ' ', '</span>' ) ;
 					$scripture = get_field('scripture');
 					$seriescover = get_field('thumbnail', 'series_'.get_the_terms( $post->ID, 'series' )[0]->term_id);
-					
+
 					if ( has_post_thumbnail() ) {
 						$imagesm = get_the_post_thumbnail_url( $post->id, 'thumbnail_uncropped' );
 						$image = get_the_post_thumbnail_url( $post->id, 'medium' );
@@ -82,7 +67,7 @@ get_header();
 					} else {
 						$imagesm = get_bloginfo('template_url').'/images/series-placeholder.png';
 						$image = $imagesm;
-					} 				
+					}
 					?>
 					<li>
 						<a href="<?php the_permalink();?>" class="cover"><img src="<?php echo $imagesm; ?>" data-layzr="<?php echo $image; ?>" alt="<?php the_title();?>"></a>
@@ -104,7 +89,7 @@ get_header();
 				<div style="padding: 40px 0; text-align: center;">It looks like there are no messages available for <?php the_archive_title(); ?>.</div>
 			<?php }
 			wp_reset_postdata();?>
-			
+
 			<div class="pagination-infinite">
 				<?php
 						global $query;
@@ -121,25 +106,26 @@ get_header();
 			<div class="page-load-status">
 				<p class="infinite-scroll-request"><img src="<?php bloginfo('template_url');?>/images/svg-loaders/tail-spin-grey.svg" alt="" class="next-page-loader" /></p>
 			</div>
-			
 		</div>
+		<?php endforeach; ?>
 	</section>
-	
-	<script src="<?php bloginfo('template_url');?>/js/vendors/infinite-scroll.pkgd.js"></script>
+
+	<!--<script src="<?php bloginfo('template_url');?>/js/vendors/infinite-scroll.pkgd.js"></script>-->
 	<script>
-		if ($('.page-numbers.next').length) {
+		/*if ($('.page-numbers.next').length) {
 			$('.series-term').infiniteScroll({
 			  path: '.page-numbers.next',
 			  append: '.series-term li',
 			  responseType: 'document',
 			  scrollThreshold: 400,
 			  history: false,
-			  hideNav: '.pagination-infinite',	
+			  hideNav: '.pagination-infinite',
 			  status: '.page-load-status',
 			  debug: false,
 			})
-		}
+		}*/
+		$('.container').not('.campus-<?php echo $campus; ?>').remove();
 	</script>
-	
+
 <?php
 get_footer();
